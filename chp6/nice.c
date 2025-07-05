204c34a462b49a46d43d5d3f1c308ec96d3c66fb
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2025-07-05 16:02:58 +0200

    Example handle process priority in UNIX systems

---
+++ chp6/nice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int ret;
+	ret = nice(0);
+	printf("Current nice by nice function: %d\n", ret);
+	ret = getpriority(PRIO_PROCESS, 0);
+	printf("Current nice by getpriority function: %d\n", ret);
+	if (setpriority(PRIO_PROCESS, 0, 10) == -1) {
+		perror("setpriority");
+		return -1;
+	}
+	ret = getpriority(PRIO_PROCESS, 0);
+	printf("Current nice by getpriority function: %d\n", ret);
+	return 0;
+}
